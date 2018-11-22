76c32d09ae56760927a66dd8287ebba804c68e71
DongGeol Kim <ehd6481@likelion.org>
2018-11-22 15:55:44 +0000

    자자

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  get 'foods/foodchallenge' => 'foods#foodchallenge'
+  get 'foods/ladder' => 'foods#ladder'
+  get 'foods/random' => 'foods#random'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
