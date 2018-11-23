e874fc31bc6c4bf9af6ab4c56d2b25687556167a
genius <conputergame@naver.com>
2018-11-23 03:08:24 +0000

    game 수정

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/foods/stuation'=>'foods#stuation'
   get '/foods/alone'=>'foods#alone'
   get '/foods/alone2'=>'foods#alone2'
+  get '/foods/game'=>'foods#game'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
